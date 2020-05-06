48655a6ec89e4ad1a0062a09858b691c3b7a4351
Bradley Arsenault <brad@kwola.io>
2020-05-05 21:02:44 -0400

    Couple fixes to the kubernetes script for the worker.

--- deployment/worker/update_worker.sh
+++ deployment/worker/update_worker.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
-sed "s/__REVISION_ID__/$REVISION_ID/g" deployment/worker/worker.yaml > worker.yaml
+sed "s/__REVISION_ID__/$REVISION_ID/g" deployment/worker/worker_deployment.yaml > worker.yaml
+kubectl apply -f worker.yaml
+
+sed "s/__REVISION_ID__/$REVISION_ID/g" deployment/worker/worker_autoscaler.yaml > worker.yaml
 kubectl apply -f worker.yaml
