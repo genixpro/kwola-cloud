3322396d2fa0b933ec1f8c5b335c75cf45737d18
Bradley Arsenault <brad@kwola.io>
2020-05-09 17:49:27 -0400

    Fixes to the updateworker scripts.

--- deployment/trainingworker/update_worker.sh
+++ deployment/trainingworker/update_worker.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/training_worker/worker_deployment.yaml > worker.yaml
+sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/trainingworker/worker_deployment.yaml > worker.yaml
 kubectl apply -f worker.yaml
 
-sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/training_worker/worker_autoscaler.yaml > worker.yaml
+sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/trainingworker/worker_autoscaler.yaml > worker.yaml
 kubectl apply -f worker.yaml
