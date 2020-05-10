613dc49af9a811b9edb12db6d08a44baad199770
Bradley Arsenault <brad@kwola.io>
2020-05-09 21:55:07 -0400

    Added in gcsfuse to the Dockerfiles.

--- deployment/api/install_system_dependencies.sh
+++ deployment/api/install_system_dependencies.sh
@@ -1,8 +1,9 @@
 #!/bin/env bash
 
 # Install node
-echo "deb http://packages.cloud.google.com/apt cloud-sdk-stretch main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | sudo tee /etc/apt/sources.list.d/gcsfuse.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 curl -sL https://deb.nodesource.com/setup_12.x | bash
 
 # Install system packages
@@ -10,31 +11,30 @@ apt-get update
 
 apt-get install \
     build-essential \
+    gfortran \
     git \
-    wget \
-    sudo \
-    vim \
+    google-cloud-sdk \
+    gcsfuse \
+    libatlas-base-dev \
+    libblas-dev \
+    libcurl4-openssl-dev \
+    liblapack-dev \
+    libnss3-tools \
+    libpng-dev \
+    libssl-dev \
+    libxml2-dev \
+    mime-support \
+    nodejs \
     python3 \
+    python3-dev \
     python3-pip \
     python3-setuptools \
     python3-tk \
-    python3-dev \
-    libpng-dev \
-    freetype* \
-    libblas-dev \
-    liblapack-dev \
-    libatlas-base-dev \
-    unzip \
-    google-cloud-sdk \
-    nodejs \
-    gfortran \
-    libcurl4-openssl-dev \
-    libxml2-dev \
-    mime-support \
-    libssl-dev \
     python3-venv \
-    libnss3-tools \
-    gfortran \
+    sudo \
+    unzip \
+    vim \
+    wget \
      -y
 
 rm -rf /var/lib/apt/lists/*
