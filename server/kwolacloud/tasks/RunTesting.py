2de70442b5e43ed456b01e24b2c21454bcc927af
Bradley Arsenault <genixpro@gmail.com>
2020-05-20 21:35:34 -0400

    Update RunTesting.py

--- server/kwolacloud/tasks/RunTesting.py
+++ server/kwolacloud/tasks/RunTesting.py
@@ -119,7 +119,7 @@ def runTesting(testingRunId):
                                        },
                                     referenceId=f"{testingRunId}-testingstep-{''.join(random.choice('abcdefghijklmnopqrstuvwxyz') for n in range(5))}",
                                     image="testingworker",
-                                    cpuRequest="2000m",
+                                    cpuRequest="1800m",
                                     cpuLimit="2500m",
                                     memoryRequest="4.0Gi",
                                     memoryLimit="5.0Gi"
