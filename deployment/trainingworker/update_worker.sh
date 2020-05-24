d7ce9cacda4f0778562f6af4d6812e167de5dead
Bradley Arsenault <brad@kwola.io>
2020-05-24 10:51:02 -0400

    removed some unused deployment files.

--- deployment/trainingworker/update_worker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/trainingworker/worker_deployment.yaml > worker.yaml
-kubectl apply -f worker.yaml
-
-sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/trainingworker/worker_autoscaler.yaml > worker.yaml
-kubectl apply -f worker.yaml
--- deployment/worker/update_worker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/worker/worker_deployment.yaml > worker.yaml
-kubectl apply -f worker.yaml
-
-sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/worker/worker_autoscaler.yaml > worker.yaml
-kubectl apply -f worker.yaml
