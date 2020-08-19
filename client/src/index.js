9c1002a51182a439afe6c2e643c8bed0a0c20e61
Bradley Arsenault <brad@kwola.io>
2020-08-19 08:44:30 -0400

    Added in sentry.io to our frontend to help catch frontend bugs.

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,14 @@ import MaterialAdmin from './materialAdmin';
 import * as serviceWorker from './serviceWorker';
 import axios from "axios";
 import mixpanel from 'mixpanel-browser';
+import * as Sentry from '@sentry/react';
 
 mixpanel.init(process.env.REACT_APP_MIXPANEL_TOKEN);
 axios.defaults.baseURL = process.env.REACT_APP_BACKEND_API_URL;
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 // axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
+Sentry.init({dsn: "https://263fefbc56504ff3b2c5ab6f7c7ba885@o436249.ingest.sentry.io/5396968"});
 
 ReactDOM.render(<MaterialAdmin />, document.getElementById('root'));
 
