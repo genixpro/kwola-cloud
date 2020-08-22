1ad923dce46b23b9b92096f14e53a1e6c21aa168
Bradley Arsenault <brad@kwola.io>
2020-08-22 18:30:23 -0400

    Changed the nginx error logging so it ignores these very common SSL handshake errors.

--- deployment/frontend/stream_to_slack.sh
+++ deployment/frontend/stream_to_slack.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 tail -n0 -F "$1" | while read LINE; do
-  (echo "$LINE" | grep -e "$3") && curl -X POST --silent --data-urlencode \
-    "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "$2";
+  if [[ ! $LINE =~ "SSL_do_handshake() failed" ]]
+  then
+    (echo "$LINE" | grep -e "$3") && curl -X POST --silent --data-urlencode \
+      "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "$2";
+  fi
 done
