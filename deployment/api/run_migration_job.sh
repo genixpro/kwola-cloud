39cc963442302edf5b384e923227c7056c207b57
Bradley Arsenault <brad@kwola.io>
2020-09-08 10:19:32 -0400

    Another minor tweak to the script which runs the migration job.

--- deployment/api/run_migration_job.sh
+++ deployment/api/run_migration_job.sh
@@ -3,6 +3,6 @@
 
 sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/api/migration_job.yaml > migration_job_$KWOLA_ENV.yaml
 kubectl delete -f migration_job_$KWOLA_ENV.yaml && exit 0
-sleep 5
+sleep 15
 kubectl create -f migration_job_$KWOLA_ENV.yaml
 
