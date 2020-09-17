30fc2641d3e02c962450503f783e4e6ec03ce008
Bradley Arsenault <brad@kwola.io>
2020-09-17 19:51:22 -0400

    Added in code as a safety net around the lazy-loading mechanism, so that it will force reload the page instead of crashing the application.

--- client/src/containers/App/appRouter.js
+++ client/src/containers/App/appRouter.js
@@ -2,94 +2,108 @@ import React, { Component, lazy, Suspense } from 'react';
 import Route from '../../components/utility/customRoute';
 import Loader from '../../components/utility/Loader/';
 
+function safeImport(moduleName)
+{
+  try
+  {
+    return import(moduleName);
+  }
+  catch(err)
+  {
+    // If there is an error lazy loading a specific page, that may mean that the underlying frontend code was updated. So
+    // just force a full reload of the page.
+    location.reload(true);
+  }
+}
+
 const routes = [
   {
     path: '',
-    component: lazy(() => import('../Dashboard')),
+    component: lazy(() => safeImport('../Dashboard')),
   },
   {
     path: 'new-application',
-    component: lazy(() => import('../NewApplication')),
+    component: lazy(() => safeImport('../NewApplication')),
   },
   {
     path: 'jira',
-    component: lazy(() => import('../JIRARedirect')),
+    component: lazy(() => safeImport('../JIRARedirect')),
   },
   {
     path: 'applications/:id/new_testing_run',
-    component: lazy(() => import('../NewTestingRun')),
+    component: lazy(() => safeImport('../NewTestingRun')),
   },
   {
     path: 'applications/:id/muted_errors',
-    component: lazy(() => import('../ViewMutedErrors')),
+    component: lazy(() => safeImport('../ViewMutedErrors')),
   },
   {
     path: 'applications/:id/notifications',
-    component: lazy(() => import('../ConfigureNotifications')),
+    component: lazy(() => safeImport('../ConfigureNotifications')),
   },
   {
     path: 'applications/:id/integrations',
-    component: lazy(() => import('../ConfigureIntegrations')),
+    component: lazy(() => safeImport('../ConfigureIntegrations')),
   },
   {
     path: 'applications/:id/webhooks',
-    component: lazy(() => import('../ConfigureWebhooks')),
+    component: lazy(() => safeImport('../ConfigureWebhooks')),
   },
   {
     path: 'applications/:id/triggers',
-    component: lazy(() => import('../ListRecurringTestingTriggers')),
+    component: lazy(() => safeImport('../ListRecurringTestingTriggers')),
   },
   {
     path: 'applications/:id/new_trigger',
-    component: lazy(() => import('../NewRecurringTestingTrigger')),
+    component: lazy(() => safeImport('../NewRecurringTestingTrigger')),
   },
   {
     path: 'applications',
-    component: lazy(() => import('../ListApplications')),
+    component: lazy(() => safeImport('../ListApplications')),
   },
   {
     path: 'applications/:id',
-    component: lazy(() => import('../ViewApplication')),
+    component: lazy(() => safeImport('../ViewApplication')),
   },
   {
     path: 'testing_sequences/:id',
-    component: lazy(() => import('../ViewTestingSequence')),
+    component: lazy(() => safeImport('../ViewTestingSequence')),
   },
   {
     path: 'training_sequences/:id',
-    component: lazy(() => import('../ViewTrainingSequence')),
+    component: lazy(() => safeImport('../ViewTrainingSequence')),
   },
   {
     path: 'training_steps/:id',
-    component: lazy(() => import('../ViewTrainingStep')),
+    component: lazy(() => safeImport('../ViewTrainingStep')),
   },
   {
     path: 'execution_sessions/:id',
-    component: lazy(() => import('../ViewExecutionSession')),
+    component: lazy(() => safeImport('../ViewExecutionSession')),
   },
   {
     path: 'execution_sessions/:id/execution_traces/:traceId',
-    component: lazy(() => import('../ViewExecutionTrace')),
+    component: lazy(() => safeImport('../ViewExecutionTrace')),
   },
   {
     path: 'testing_runs/:id/configuration',
-    component: lazy(() => import('../ViewTestingRunConfiguration')),
+    component: lazy(() => safeImport('../ViewTestingRunConfiguration')),
   },
   {
     path: 'testing_runs/:id',
-    component: lazy(() => import('../ViewTestingRun')),
+    component: lazy(() => safeImport('../ViewTestingRun')),
   },
   {
     path: 'bugs/:id',
-    component: lazy(() => import('../ViewBug')),
+    component: lazy(() => safeImport('../ViewBug')),
   },
   {
     path: 'triggers/:id',
-    component: lazy(() => import('../ViewRecurringTestingTrigger')),
+    component: lazy(() => safeImport('../ViewRecurringTestingTrigger')),
   },
   {
     path: 'triggers/:id/configuration',
-    component: lazy(() => import('../ViewRecurringTestingTriggerConfiguration')),
+    component: lazy(() => safeImport('../ViewRecurringTestingTriggerConfiguration')),
   }
 ];
 
