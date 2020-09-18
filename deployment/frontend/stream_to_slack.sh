a8197c2211bfe73f2420a19ba324c9c988cb26e4
Bradley Arsenault <brad@kwola.io>
2020-09-17 21:24:26 -0400

    Added code to disable the nginx slack errors from streaming to slack from the testing environment.

--- deployment/frontend/stream_to_slack.sh
+++ deployment/frontend/stream_to_slack.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-tail -n0 -F "$1" | while read LINE; do
-  if [[ ! $LINE =~ "SSL_do_handshake() failed" ]]
-  then
-    (echo "$LINE" | grep -e "$3") && curl -X POST --silent --data-urlencode \
-      "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "$2";
-  fi
-done
+if [[ $KWOLA_ENV != "testing" ]]; then
+  tail -n0 -F "$1" | while read LINE; do
+    if [[ ! $LINE =~ "SSL_do_handshake() failed" ]]
+    then
+      (echo "$LINE" | grep -e "$3") && curl -X POST --silent --data-urlencode \
+        "payload={\"text\": \"$(echo $LINE | sed "s/\"/'/g")\"}" "$2";
+    fi
+  done
+fi
