0d650e03918a67e2db677392e9c483af7789d77a
Bradley Arsenault <brad@kwola.io>
2020-09-25 13:13:23 -0400

    Changed the imagePullPolicy to Always in kubernetes.

--- server/kwolacloud/components/utils/KubernetesJob.py
+++ server/kwolacloud/components/utils/KubernetesJob.py
@@ -83,6 +83,7 @@ class KubernetesJob:
                                 "image": f"gcr.io/kwola-cloud/kwola-{self.image}-{os.getenv('KWOLA_ENV')}:latest",
                                 "command": ["/usr/bin/python3"],
                                 "args": ["-m", str(self.module), str(base64.b64encode(pickle.dumps((self.kubeJobName(), self.data)), altchars=KubernetesJobProcess.base64AltChars), 'utf8')],
+                                "imagePullPolicy": "Always",
                                 "securityContext": {
                                     "privileged": True,
                                     "capabilities":
