f579242243b352150b512c43bdb25fc66da2f143
Bradley Arsenault <brad@kwola.io>
2020-10-30 13:44:46 -0400

    Fixed the reference to the demo reset job.

--- deployment/api/update_demo_reset.sh
+++ deployment/api/update_demo_reset.sh
@@ -2,7 +2,7 @@
 
 if [[ "$KWOLA_ENV" == "testing" || "$KWOLA_ENV" == "demo" ]] ;
     then
-    sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/api/demo_reset_job.yaml > demo_reset_job_$KWOLA_ENV.yaml
-    kubectl apply -f demo_reset_job_$KWOLA_ENV.yaml
+    sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/api/db_reset_job.yaml > db_reset_job_$KWOLA_ENV.yaml
+    kubectl apply -f db_reset_job_$KWOLA_ENV.yaml
 fi
 
