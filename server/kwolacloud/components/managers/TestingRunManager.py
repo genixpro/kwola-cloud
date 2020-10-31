36028a3ebe6a39a0693589b255014a83310db1d8
Bradley Arsenault <brad@kwola.io>
2020-10-31 11:36:31 -0400

    Fixed a mistake in the code that limits the maximum parallel sessions.

--- server/kwolacloud/components/managers/TestingRunManager.py
+++ server/kwolacloud/components/managers/TestingRunManager.py
@@ -176,7 +176,7 @@ class TestingRunManager:
     def launchTestingStepsIfNeeded(self):
         logging.info(f"Launching testing steps if needed. Number to launch: {self.calculateNumberOfTestingSessionsToStart()}")
         runningSessions = len(self.run.runningTestingStepJobIds) * self.config['web_session_parallel_execution_sessions']
-        while self.calculateNumberOfTestingSessionsToStart() > 0 and runningSessions < self.run.maxParallelSessions:
+        while self.calculateNumberOfTestingSessionsToStart() > 0 and runningSessions < self.run.configuration.maxParallelSessions:
             self.launchTestingStep()
 
 
