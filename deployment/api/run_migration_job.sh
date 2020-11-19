6ac992af4a1fd210144343d384cae621ae86651d
Bradley Arsenault <brad@kwola.io>
2020-11-19 14:54:42 -0500

    Added in a configuration for a 'demobackup' environment that allows the migration job to run on the backup data for the demo environment.

--- deployment/api/run_migration_job.sh
+++ deployment/api/run_migration_job.sh
@@ -5,3 +5,10 @@
 sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/$KWOLA_ENV/g" deployment/api/migration_job.yaml > migration_job_$KWOLA_ENV.yaml
 kubectl delete -f migration_job_$KWOLA_ENV.yaml
 kubectl create -f migration_job_$KWOLA_ENV.yaml
+
+if [[ "$KWOLA_ENV" == "demo" ]] ;
+    then
+    sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/demobackup/g" deployment/api/migration_job.yaml > migration_job_demobackup.yaml
+    kubectl delete -f migration_job_demobackup.yaml
+    kubectl create -f migration_job_demobackup.yaml
+fi
