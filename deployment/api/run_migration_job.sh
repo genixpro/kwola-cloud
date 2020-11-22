369f31fc3a9f1e11334fa499dc2ea03bc0d01f5a
Bradley Arsenault <brad@kwola.io>
2020-11-22 17:10:25 -0500

    Another fix to the demobackup migration job.

--- deployment/api/run_migration_job.sh
+++ deployment/api/run_migration_job.sh
@@ -11,6 +11,7 @@ if [[ "$KWOLA_ENV" == "demo" ]] ;
     kubectl apply -f deployment/priorities/demobackup.yaml
     kubectl apply -f deployment/priorities/demobackup_background.yaml
     sed "s/__REVISION_ID__/$REVISION_ID/g;s/__KWOLA_ENV__/demobackup/g" deployment/api/migration_job.yaml > migration_job_demobackup.yaml
+    sed "s/kwola-api-demobackup/kwola-api-demo/g" -i migration_job_demobackup.yaml
     kubectl delete -f migration_job_demobackup.yaml
     kubectl create -f migration_job_demobackup.yaml
 fi
